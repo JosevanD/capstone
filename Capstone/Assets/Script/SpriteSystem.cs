43cf4cd90491e15ee89f0f031c62f96a9322ee4b
JosevanD <josevan.danu@gmail.com>
2023-08-10 18:20:51 +1000

    Sorting Layer + Backwalking
    - Configured Sorting Layer
    - Added Back walking animation

--- Capstone/Assets/Script/SpriteSystem.cs
+++ Capstone/Assets/Script/SpriteSystem.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class SpriteSystem : MonoBehaviour
 {
     [SerializeField] bool freezeXZAxis = true;
 
     [SerializeField] Animator spriteAnimator;
+
+    [SerializeField] GameObject walkAudioObject;
+
+    bool isWalking = false;
+    //bool audioToggle = true;
 
     void Update()
     {
@@ -15,6 +21,8 @@ public class SpriteSystem : MonoBehaviour
 
         SpriteAnim();
 
+
+        
     }
 
     private void Billboard()
@@ -35,39 +43,58 @@ public class SpriteSystem : MonoBehaviour
         if (Input.GetKey(KeyCode.S))
         {
             spriteAnimator.SetBool("FrontWalk", true);
-
         }
 
         else
         {
             spriteAnimator.SetBool("FrontWalk", false);
-
         }
 
         if (Input.GetKey(KeyCode.D))
         {
             spriteAnimator.SetBool("RightWalk", true);
-
         }
 
         else
         {
-        spriteAnimator.SetBool("RightWalk", false);
-
+            spriteAnimator.SetBool("RightWalk", false);
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-        spriteAnimator.SetBool("LeftWalk", true);
-
+            spriteAnimator.SetBool("LeftWalk", true);
         }
 
         else
         {
-        spriteAnimator.SetBool("LeftWalk", false);
+            spriteAnimator.SetBool("LeftWalk", false);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            spriteAnimator.SetBool("BackWalk", true);
+        }
+
+        else
+        {
+            spriteAnimator.SetBool("BackWalk", false);
+        }
+    }
+
+
+    private void Walking()
+    {
+        if ((Input.GetKey(KeyCode.W)) || (Input.GetKey(KeyCode.A)) || (Input.GetKey(KeyCode.S)) || (Input.GetKey(KeyCode.D)))
+        {
+            //isWalking = true;
+            walkAudioObject.SetActive(true);
 
         }
-        
-
+        else
+        {
+            //walkAudio.SetActive = false;
+            walkAudioObject.SetActive(false);
+        }
+     
     }
 }
