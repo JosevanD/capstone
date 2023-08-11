47a91b68fe8883c2ff07f32d2f530996d2f99eae
JosevanD <josevan.danu@gmail.com>
2023-08-11 18:01:11 +1000

    Created Door Anim + Sound Stuff
    - Added Door_Prefab to travel to white hallway
    - Changed Character Controller a bit to modify sound

---
+++ Capstone/Assets/Shaders/Door Shader/DissolveScript.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DissolveScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public MeshRenderer mesh;
+    public float dissolveRate = 0.0125f;
+    public float refreshRate = 0.025f;
+
+    private Material[] meshMaterials;
+
+    void Start()
+    {
+        if (mesh != null)
+        {
+            meshMaterials = mesh.materials;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //StartCoroutine(DissolveCor());
+            StartCoroutine(MaterializeCor());
+        }
+    }
+
+    IEnumerator DissolveCor()
+    {
+        if (meshMaterials.Length > 0)
+        {
+            float counter = 0;
+
+            while (meshMaterials[0].GetFloat("_DissolveAmount") < 1)
+            {
+                counter += dissolveRate;
+                for (int i=0; i<meshMaterials.Length; i++)
+                {
+                    meshMaterials[i].SetFloat("_DissolveAmount", counter);
+                }
+                yield return new WaitForSeconds(refreshRate);
+            }
+
+        }
+    }
+
+    IEnumerator MaterializeCor()
+    {
+        if (meshMaterials.Length > 0)
+        {
+            float counter = 0;
+
+            while (meshMaterials[0].GetFloat("_DissolveAmount") > 0)
+            {
+                counter += dissolveRate;
+                for (int i = 0; i < meshMaterials.Length; i++)
+                {
+                    meshMaterials[i].SetFloat("_DissolveAmount", (1 - counter));
+                }
+                yield return new WaitForSeconds(refreshRate);
+            }
+
+        }
+    }
+
+}
