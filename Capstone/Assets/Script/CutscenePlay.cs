8353a78431da5d99179f83ba9001de2952711f8f
JosevanD <josevan.danu@gmail.com>
2023-08-15 23:03:40 +1000

    Door 5 Cutscene + Hallway Changes
    - Added Door 5 Cutscene
    - Changed cutscene script to add more functions
    - Added global volume to Door 5
    - Changed Doors in hallway

--- Capstone/Assets/Script/CutscenePlay.cs
+++ Capstone/Assets/Script/CutscenePlay.cs
@@ -7,38 +7,76 @@ public class CutscenePlay : MonoBehaviour
     [Header("Cutscenes")]
     public GameObject cutsceneObject;
     public float cutsceneTime;
+    public bool cutscenePlayOnce = true;
+    
 
     [Header("Animation")]
     public Animator precutsceneAnim;
     public string animPara;
+    public bool hasAnimation;
    
     [Header("Audio")]
     public AudioSource cutsceneAudio;
     public GameObject bgAudioObject;
 
+    [Header("Misc")]
+    public GameObject objToActivate;
+    public GameObject objToDeactivate;
+    public bool hasObjToActivate = false;
+    public bool hasObjToDectivate = false;
+
     private void OnTriggerEnter(Collider collider)
     {
-        if (collider.tag == "Player")
+        if (collider.tag == "Player" && hasAnimation == true)
         {
-            precutsceneAnim.SetBool(animPara, true);
+            precutsceneAnim.SetBool(animPara, true);   
         }
+
+        if (collider.tag == "Player" && hasAnimation == false)
+        {
+            
+            CutsceneOn();
+        }
+
     }
 
     public void CutsceneOn()
     {
+        //pauses BG Music, sets cutscene on and cutscene audio on
         bgAudioObject.GetComponent<BackgroundAudioController>().PauseBGAudio();
         cutsceneObject.SetActive(true);
         cutsceneAudio.enabled = true;
+
+        //deactivates an object
+        if (hasObjToDectivate == true)
+        {
+            objToDeactivate.SetActive(false);
+        }
+
+        //starts timer for cutscene music and off
         StartCoroutine(CutsceneOff(cutsceneTime));
     }
 
     IEnumerator CutsceneOff(float time)
     {
-        
+        //wait for seconds
         yield return new WaitForSeconds(time);
 
+        //disable audio, cutscene and resume BG Audio
         cutsceneAudio.enabled = false;
         cutsceneObject.SetActive(false);
         bgAudioObject.GetComponent<BackgroundAudioController>().ResumeBGAudio();
+
+        //activate any objects
+        if (hasObjToActivate == true)
+        {
+            objToActivate.SetActive(true);
+        }
+        
+
+        if (cutscenePlayOnce == true)
+        {
+            Destroy(gameObject);
+        }
     }
 }
---
+++ Capstone/Assets/Shaders/Door Shader/TriggerDissolve.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDissolve : MonoBehaviour
+{
+    public GameObject doorToDissolve;
+    public GameObject doorToAppear;
+    public bool isDissolve;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+         
+    }
+}
