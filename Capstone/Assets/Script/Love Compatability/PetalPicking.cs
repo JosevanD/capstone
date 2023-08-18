ea4d62795eea8354247b2abc6899fb7de776b0b6
Issac Mao S3790625 <1518872279@qq.com>
2023-08-18 19:56:24 +1000

    Added Dragging into flower picking

--- Capstone/Assets/Script/Love Compatability/PetalPicking.cs
+++ Capstone/Assets/Script/Love Compatability/PetalPicking.cs
@@ -3,21 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class PetalPicking : MonoBehaviour, IPointerDownHandler
+public class PetalPicking : MonoBehaviour
 {
     FlowerManager flowerManager;
+    //private float DraggingTimer;
+    //public float MaxDraggingTime;
     // Start is called before the first frame update
     void Start()
     {
         flowerManager = FindObjectOfType<FlowerManager>();
     }
 
-    public void OnPointerDown(PointerEventData eventData)
+    /*public void OnPointerDown(PointerEventData eventData)
     {
         flowerManager.PickedFlowerPetals++;
         
         Destroy(gameObject);
     
+    }*/
+
+    public void OnMouseDrag()
+    {
+        gameObject.transform.position = Input.mousePosition;
+
+
+    }
+
+    public void OnMouseUp()
+    {
+        Destroy(gameObject);
+
     }
 
 
