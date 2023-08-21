c136aeb4d0056c1190873553c82c9a22ce28dc0c
Issac Mao S3790625 <1518872279@qq.com>
2023-08-22 00:55:11 +1000

    Changed the flowerpicking mechanics and added sprites

--- Capstone/Assets/Script/Love Compatability/FlowerManager.cs
+++ Capstone/Assets/Script/Love Compatability/FlowerManager.cs
@@ -1,34 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FlowerManager : MonoBehaviour
 {
-
+    [SerializeField]
+    private GameObject FlowerPickingCanvasObj;
+    //public Image 
+    public int MaxEndingTime;
     public GameObject[] FlowerPetals;
     private int FlowerPetalsCounts;
-    public int PickedFlowerPetals;
+    public int CurrFlowerPetal;
+
+    //public int PickedFlowerPetals;
+    public GameObject EndingDoorObj;
     private bool isFinished;
     // Start is called before the first frame update
     void Start()
     {
+        CurrFlowerPetal = 0;
         isFinished = false;
-        PickedFlowerPetals = 0;
+        //PickedFlowerPetals = 0;
         //FlowerPetalsCounts = GameObject.FindGameObjectsWithTag("Flower Petal").Length;
         FlowerPetalsCounts = FlowerPetals.Length;
+        foreach (var petalObject in FlowerPetals)
+        {
+            petalObject.GetComponent<Image>().raycastTarget = false;
+        }
+        
+    }
+    IEnumerator Countdown(int seconds)
+    {
+        int counter = seconds;
+        while (counter > 0)
+        {
+            yield return new WaitForSeconds(1);
+            counter--;
+        }
+
+        FlowerPickingCanvasObj.SetActive(false);
+        EndingDoorObj.SetActive(true);
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (PickedFlowerPetals >= FlowerPetalsCounts)
+        if (CurrFlowerPetal >= FlowerPetalsCounts)
         {
             //puzzle ends
+            
+            StartCoroutine(Countdown(MaxEndingTime));
             Debug.Log("Flower Picking Finished");
         
         }
-
-
+        if (CurrFlowerPetal < FlowerPetalsCounts)
+            //FlowerPetals[CurrFlowerPetal].GetComponent<PetalPicking>().isPickable = true;
+            FlowerPetals[CurrFlowerPetal].GetComponent<Image>().raycastTarget = true;
     }
 }
--- Capstone/Assets/Script/Love Compatability/PetalPicking.cs
+++ Capstone/Assets/Script/Love Compatability/PetalPicking.cs
@@ -1,17 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 public class PetalPicking : MonoBehaviour
 {
     FlowerManager flowerManager;
+
+    [SerializeField]
+    private Canvas FlowerPickingCanvas;
+    //public bool isPickable;
     //private float DraggingTimer;
     //public float MaxDraggingTime;
     // Start is called before the first frame update
     void Start()
     {
         flowerManager = FindObjectOfType<FlowerManager>();
+        //isPickable = false;
     }
 
     /*public void OnPointerDown(PointerEventData eventData)
@@ -21,6 +27,22 @@ public class PetalPicking : MonoBehaviour
         Destroy(gameObject);
     
     }*/
+    public void DragHandler(BaseEventData data)
+    {
+        
+        PointerEventData pointerData = (PointerEventData)data;
+        Vector2 position;
+
+        RectTransformUtility.ScreenPointToLocalPointInRectangle((RectTransform)FlowerPickingCanvas.transform,
+            pointerData.position,
+            FlowerPickingCanvas.worldCamera,
+            out position
+            );
+        transform.position = FlowerPickingCanvas.transform.TransformPoint(position);
+
+
+    
+    }
 
     public void OnMouseDrag()
     {
@@ -31,7 +53,12 @@ public class PetalPicking : MonoBehaviour
 
     public void OnMouseUp()
     {
+
+
+        flowerManager.CurrFlowerPetal++;
         Destroy(gameObject);
+
+
 
     }
 
