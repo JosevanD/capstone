65e581ebd1c2319dae0aeadc9fd4bc53bed390eb
Issac Mao S3790625 <1518872279@qq.com>
2023-11-05 21:04:16 +1100

    added slide down sfx for slide

--- Capstone/Assets/Script/Door 1 Scripts/SlideTeleport.cs
+++ Capstone/Assets/Script/Door 1 Scripts/SlideTeleport.cs
@@ -4,10 +4,31 @@ using UnityEngine;
 
 public class SlideTeleport : MonoBehaviour
 {
+    [Header("Slide SFX")]
+    private AudioSource SlideAudioSource;
+    public AudioClip SlideDownClip;
+
     [SerializeField] GameObject player;
     [SerializeField] GameObject teleportObj;
+
+    private void Start()
+    {
+        
+        SlideAudioSource = GetComponent<AudioSource>();
+    }
     public void PlayerTeleportSlide()
     {
         player.transform.position = teleportObj.transform.position;
+    }
+
+    public void PlaySlideDownClip()
+    {
+        SlideAudioSource.PlayOneShot(SlideDownClip);
+    
+    }
+
+    public void StopClip()
+    {
+        SlideAudioSource.Stop();
     }
 }
