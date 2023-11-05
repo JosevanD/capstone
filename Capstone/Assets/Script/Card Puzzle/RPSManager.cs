8a0eb4fdc2c1e3ceb2c50f91b35563c61dd49df5
Issac Mao S3790625 <1518872279@qq.com>
2023-11-05 22:01:58 +1100

    polished the card battle puzzle

--- Capstone/Assets/Script/Card Puzzle/RPSManager.cs
+++ Capstone/Assets/Script/Card Puzzle/RPSManager.cs
@@ -8,6 +8,10 @@ public class RPSManager : MonoBehaviour
 {
     
     public TMP_Text Result;
+    public TMP_Text playerScore;
+    public TMP_Text EnemyScore;
+    private int playerWinCount;
+    private int enemyWinCount;
     public Image AIChoice;
 
     public string[] Choices;
@@ -35,25 +39,30 @@ public class RPSManager : MonoBehaviour
         Result.text = "Choose Your Card From The Right Pile";
         CurrWinedRound = 0;
         isAIWon = false;
+        playerWinCount = 0;
+        enemyWinCount = 0;
         
     }
     private void Update()
     {
         if (CurrWinedRound >= TotalWinedRound)
         {
-            Debug.Log("complete");
+            /*Debug.Log("complete");
             Result.text = "You Won the Battle!";
             CardPuzzleGameobject.SetActive(false);
             
             TheEndingDoorObj.SetActive(true);
-            charactorController.isInteracting = false;
+            charactorController.isInteracting = false;*/
+            StartCoroutine(Countdown(3));
         }
+        playerScore.text = "" +  playerWinCount;
+        EnemyScore.text = "" + enemyWinCount;
     }
     public void Play(string myChoice)
     {
         string randomChoice = Choices[Random.Range(0, Choices.Length)];
 
-        if (isAIWon == false)
+        if (enemyWinCount < 2)
         {
             switch (randomChoice)
             {
@@ -66,11 +75,13 @@ public class RPSManager : MonoBehaviour
 
                         case "Paper":
                             Result.text = "The paper covers the rock, you win!";
+                            playerWinCount++;
                             CurrWinedRound++;
                             break;
 
                         case "Scissors":
                             Result.text = "The rock destroys the scissors, you lose!";
+                            enemyWinCount++;
                             isAIWon = true;
                             break;
 
@@ -84,6 +95,7 @@ public class RPSManager : MonoBehaviour
                     {
                         case "Rock":
                             Result.text = "The paper covers the rock, you lose!";
+                            enemyWinCount++;
                             isAIWon = true;
                             break;
 
@@ -93,6 +105,7 @@ public class RPSManager : MonoBehaviour
 
                         case "Scissors":
                             Result.text = "The scissors cuts the paper, you win!";
+                            playerWinCount++;
                             CurrWinedRound++;
                             break;
 
@@ -106,11 +119,13 @@ public class RPSManager : MonoBehaviour
                     {
                         case "Rock":
                             Result.text = "The rock destroys the scissors, you win!";
+                            playerWinCount++;
                             CurrWinedRound++;
                             break;
 
                         case "Paper":
                             Result.text = "The scissors cuts the paper, you Lose!";
+                            enemyWinCount++;
                             isAIWon = true;
                             break;
 
@@ -129,25 +144,28 @@ public class RPSManager : MonoBehaviour
         }
 
         //Debug.Log("isWined " + isAIWined);
-        else if (isAIWon == true) 
+        else if (enemyWinCount == 2) 
         {
             
             switch (myChoice)
         {
             case "Rock":
                 Result.text = "The rock destroys the scissors, you win!";
+                playerWinCount++;
                 CurrWinedRound++;
                 AIChoice.sprite = AIScissors;
                 break;
 
             case "Paper":
                 Result.text = "The paper covers the rock, you win!";
+                playerWinCount++;
                 CurrWinedRound++;
                 AIChoice.sprite = AIRock;
                 break;
 
             case "Scissors":                 
                 Result.text = "The scissors cuts the paper, you win!";
+                playerWinCount++;
                 CurrWinedRound++;
                 AIChoice.sprite = AIPaper;
                 break;
@@ -156,5 +174,23 @@ public class RPSManager : MonoBehaviour
         }
     }
 
-   
+    IEnumerator Countdown(int seconds)
+    {
+        int counter = seconds;
+        while (counter > 0)
+        {
+            yield return new WaitForSeconds(1);
+            counter--;
+        }
+
+        Debug.Log("complete");
+        Result.text = "You Won the Battle!";
+        CardPuzzleGameobject.SetActive(false);
+
+        TheEndingDoorObj.SetActive(true);
+        charactorController.isInteracting = false;
+
+    }
+
+
 }
