a379b31e284bb3e9dd790c1de07c9ffcbfeaf7ce
Issac Mao S3790625 <1518872279@qq.com>
2023-11-06 01:19:19 +1100

    added SFX for slide

--- Capstone/Assets/Script/Door 1 Scripts/SlideTeleport.cs
+++ Capstone/Assets/Script/Door 1 Scripts/SlideTeleport.cs
@@ -7,13 +7,19 @@ public class SlideTeleport : MonoBehaviour
     [Header("Slide SFX")]
     private AudioSource SlideAudioSource;
     public AudioClip SlideDownClip;
+    public AudioClip SlideStepClip1;
+    public AudioClip SlideStepClip2;
+    public AudioClip SlideStepClip3;
+    public AudioClip SlideStepClipWhole;
+
+
 
     [SerializeField] GameObject player;
     [SerializeField] GameObject teleportObj;
 
     private void Start()
     {
-        
+
         SlideAudioSource = GetComponent<AudioSource>();
     }
     public void PlayerTeleportSlide()
@@ -24,11 +30,36 @@ public class SlideTeleport : MonoBehaviour
     public void PlaySlideDownClip()
     {
         SlideAudioSource.PlayOneShot(SlideDownClip);
-    
+
     }
 
     public void StopClip()
     {
         SlideAudioSource.Stop();
+    }
+
+    public void PlaySlideStepClip1()
+    {
+        StopClip();
+        SlideAudioSource.PlayOneShot(SlideStepClip1);
+
+    }
+    public void PlaySlideStepClip2()
+    {
+        StopClip();
+        SlideAudioSource.PlayOneShot(SlideStepClip2);
+
+    }
+    public void PlaySlideStepClip3()
+    {
+        StopClip();
+        SlideAudioSource.PlayOneShot(SlideStepClip3);
+
+    }
+    public void PlaySlideStepClipWhole()
+    {
+        StopClip();
+        SlideAudioSource.PlayOneShot(SlideStepClipWhole);
+
     }
 }
