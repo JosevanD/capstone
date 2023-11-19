31b1aa60e44c3286ea352362debc18f3547654c5
JosevanD <josevan.danu@gmail.com>
2023-11-19 23:19:50 +1100

    Door2 Reward thing

--- Capstone/Assets/Script/Door 2 Scripts/Door2Teleport.cs
+++ Capstone/Assets/Script/Door 2 Scripts/Door2Teleport.cs
@@ -7,12 +7,12 @@ public class Door2Teleport : MonoBehaviour
     [SerializeField] GameObject player;
     [SerializeField] GameObject teleportObj;
     [SerializeField] bool inTrigger;
-
+    private GameObject sceneTrackerObj;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        sceneTrackerObj = GameObject.Find("SceneTracker");
     }
 
     // Update is called once per frame
@@ -27,7 +27,9 @@ public class Door2Teleport : MonoBehaviour
 
     public void PlayerTeleportDoor2()
     {
+        sceneTrackerObj.GetComponent<SceneTracker>().RewardCollected();
         player.transform.position = teleportObj.transform.position;
+        
     }
 
 }
